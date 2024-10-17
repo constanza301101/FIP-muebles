4b6763793050e453a02ce2ac78d50078e759b784
Martín <136357272+07martin12@users.noreply.github.com>
2024-10-17 20:25:08 -0300

    "se crea el archivo script.ts"

--- readme.md
+++ readme.md
@@ -3,3 +3,5 @@ Los usuarios pueden registrarse en el sitio y crear una cuenta para realizar sus
 El sitio tiene una barra de navegación para buscar un mueble por categoría.
 El usuario puede guardar en una lista los muebles que quiere comprar. 
  El usuario tiene múltiples opciones de pago. 
+
+
---
+++ script.ts
@@ -0,0 +1,33 @@
+import * as rls from 'readline-sync';
+
+registrarUsuario ();
+navegacion ();
+realizarCompra ();
+
+function registrarUsuario () {
+    let nombre: String;
+    let ciudad: String;
+    let edad: number;
+    let año: number;
+    let mes: number;
+    let dia: number;
+    let metodoDePago: number;
+
+    //se pide al usuario que ingrese sus datos y se guardan en las variables
+
+    
+}
+
+function navegacion () {
+    //El sitio tiene una barra de navegación para buscar un mueble por categoría.
+}
+
+function realizarCompra () {
+    //El usuario puede guardar en una lista los muebles que quiere comprar. 
+    //El usuario tiene múltiples opciones de pago. 
+
+
+    //lista numerica de opciones de pago y un if-else if
+}
+
+
