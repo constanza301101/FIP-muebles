4ee52c6512b67bc2c24d273356b9bd3c6256ec32
constanza301101 <70646911+constanza301101@users.noreply.github.com>
2024-10-17 20:35:57 -0300

    funcion pago y favoritos

--- script.ts
+++ script.ts
@@ -3,6 +3,7 @@ import * as rls from 'readline-sync';
 registrarUsuario ();
 navegacion ();
 realizarCompra ();
+favoritos();
 
 function registrarUsuario () {
     let nombre: String;
@@ -26,8 +27,26 @@ function realizarCompra () {
     //El usuario puede guardar en una lista los muebles que quiere comprar. 
     //El usuario tiene múltiples opciones de pago. 
 
-
     //lista numerica de opciones de pago y un if-else if
+
+    //llame a una funcion que le pida al usuario con que va a pagar que le pasa por parametro con que va a pagar
+    if(vaAPagarCon=="tarjeta"){
+        //pedir si va a pagar con debito o credito
+        if(debito){
+            //pedir los datos de la tarjeta
+        }
+        else if(credito){
+            //pedir los datos de la tarjeta
+            //y en cuantas cuotas lo va a hacer
+        }
+    }
+    else if(transferencia){
+        //mostrar por pantalla el CBU o alias
+    }
+}
+function favoritos(){
+    //if usuario === like
+    //añadir al arreglo
 }
 
 
